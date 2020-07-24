6612dff65b96ca6fb49e42c2047b69d1a89a2983
Nitish279 <nitish4u029@gmail.com>
2020-07-25 01:15:27 +0530

    initial commit

---
+++ Scripts/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    var url = window.location.pathname;
+    var substr = url.split('index.html');
+    var urlaspx = substr[substr.length - 1];
+    //Change this Code to Accomodate TimeSheet Types
+    $('.nav').find('.active').removeClass('active');
+    $('.nav li a').each(function() {
+        if (this.href.indexOf(urlaspx) >= 0) {
+            $(this).parent().addClass('active');
+        }
+    });
+
+    function GetFileSizeNameAndType() {
+        var fi = document.getElementById('file'); // GET THE FILE INPUT AS VARIABLE.
+
+        var totalFileSize = 0;
+
+        // VALIDATE OR CHECK IF ANY FILE IS SELECTED.
+        if (fi.files.length > 0) {
+            // RUN A LOOP TO CHECK EACH SELECTED FILE.
+            for (var i = 0; i <= fi.files.length - 1; i++) {
+                //ACCESS THE SIZE PROPERTY OF THE ITEM OBJECT IN FILES COLLECTION. IN THIS WAY ALSO GET OTHER PROPERTIES LIKE FILENAME AND FILETYPE
+                var fsize = fi.files.item(i).size;
+                totalFileSize = totalFileSize + fsize;
+                document.getElementById('fp').innerHTML =
+                    document.getElementById('fp').innerHTML +
+                    '<br /> ' + 'File Name is <b>' + fi.files.item(i).name +
+                    '</b> and Size is <b>' + Math.round((fsize / 1024)) //DEFAULT SIZE IS IN BYTES SO WE DIVIDING BY 1024 TO CONVERT IT IN KB
+                    +
+                    '</b> KB and File Type is <b>' + fi.files.item(i).type + "</b>.";
+            }
+        }
+        document.getElementById('divTotalSize').innerHTML = "Total File(s) Size is <b>" + Math.round(totalFileSize / 1024) + "</b> KB";
+    }
+});
