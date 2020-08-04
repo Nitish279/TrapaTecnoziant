06b6d3c20c2a26393c5b9fe7eca390279b154cc8
Paurush Ankit <paurush.ankit@freighttiger.com>
2020-08-04 14:42:20 +0530

    Added modal

--- mail.php
+++ mail.php
@@ -25,8 +25,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'TradingGyan';                     // SMTP username
-    $mail->Password   = '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';                               // SMTP password
+    $mail->Username   = 'n.nitish131@gmail.com';                     // SMTP username
+    $mail->Password   = 'Nitish@7784';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -48,8 +48,10 @@ try {
                     </ul>";
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
+    // $mail->send();
+    header('Location: index.html?success=true');
+
+    // echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: ";
 }
